09df34399921c33079ab48809cd3a4600fb989ae
wpf5511 <wpf5511@gmail.com>
2016-08-09 06:13:25 +0000

    add netophrase

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
+#include <locale>
+
 #include <vector>
 #include <map>
 #include <stdlib.h>
-#include <map>
+#include <cmath>
+#include <codecvt>
 #include "rapidxml.hpp"
 #include "rapidxml_print.hpp"
 #include "rapidxml_utils.hpp"
@@ -24,6 +28,24 @@ std::string readFile(std::string filePath){
 
     return outss.str();
 }
+
+std::wstring s2ws(const std::string& str)
+{
+    using convert_typeX = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+    return converterX.from_bytes(str);
+}
+
+std::string ws2s(const std::wstring& wstr)
+{
+    using convert_typeX = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+    return converterX.to_bytes(wstr);
+}
+
+
 
 struct Token{
     int sentenceid;
@@ -62,8 +84,93 @@ void extractNEP(std::vector<Token> tokens,std::map<int,NEPMAP>& senNEP){
 class Phrase;
 //Phrase add attribute string NEtype  bool isNE
 void addNEtoPhrase(NEPMAP neps,std::map<int,Phrase>&phrases){
+    for(auto it = phrases.begin();it!=phrases.end();it++){
+        if(it->second.isArgument== false){
+            continue;
+        } else{
+            int  phead = it->second.head;
+            std::string pheadstring = ztree.getNode(phead).word;
 
+            int maxlenlcs = 0;
+            NEPMAP::iterator maxlenitor;
+            for(auto it2= neps.begin();it2!=neps.end();it2++){
+                int neHead = it2->first;
+                std::string neHeadstring = it2->second.headstring;
+
+                std::wstring wphead = s2ws(pheadstring);
+
+                std::wstring wneHead = s2ws(neHeadstring);
+                if(std::fabs(phead-neHead)<=3&&wphead.back()==wneHead.back()){
+                    it->second.entityType = it2->second.entityType;
+                    neps.erase(it2);
+                    break;
+                }else{
+                    std::string pContent = it->second.content;
+                    std::string neContent = it2->second.phrase;
+
+                    int commonlen = lcs(s2ws(pContent),s2ws(neContent));
+
+                    if(commonlen>maxlenlcs){
+                        maxlenlcs = commonlen;
+                        maxlenitor = it2;
+                    }
+                }
+            }
+            if(maxlenlcs>0){
+                neps.erase(maxlenitor);
+            }
+        }
+    }
 }
+
+
+
+
+int lcs(std::wstring A,std::wstring B){
+    std::vector<std::vector<int> > len;
+    len.resize(A.size()+1);
+    for(int i = 0;i <= A.size();i++){
+        len[i].resize(B.size()+1,0);
+    }
+    for (int i = 1; i <= A.size(); ++i)
+    {
+        for (int j = 1; j <= B.size(); ++j)
+        {
+            if (A[i-1] == B[j-1])
+            {
+                len[i][j] = len[i-1][j-1] + 1;
+            }else if (len[i-1][j] >= len[i][j-1])
+            {
+                len[i][j] = len[i-1][j];
+            }else{
+                len[i][j] = len[i][j-1];
+            }
+        }
+    }
+    int apos = A.size();
+    int bpos = B.size();
+    int commonlen = len[apos][bpos];
+    int k = commonlen;
+    /*common.resize(commonlen);
+    while(apos && bpos){
+        if(len[apos][bpos] == len[apos-1][bpos] + 1){
+            common[--k] = A[--apos];
+            --bpos;
+        }else if (len[apos-1][bpos] >= len[apos][bpos-1])
+        {
+            --apos;
+        }else{
+            --bpos;
+        }
+    }
+    for(int i = 0;i < commonlen;i++){
+        cout<<common[i];
+    }
+    cout<<endl;*/
+    return commonlen;
+}
+
+
 
 int main() {
 
